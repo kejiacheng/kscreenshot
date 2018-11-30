3d4c498d4de841c7a2b63b49ac69ad50c42bb7fc
柯嘉诚 <kc375615453@qq.com>
2018-11-30 09:49:27 +0800

    none

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack')
+ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
